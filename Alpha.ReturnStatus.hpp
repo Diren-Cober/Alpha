61f08f3f84e28a280289dcf5fa7f1f87f301cd61
Diren-Cober <56950456+Diren-Cober@users.noreply.github.com>
2020-03-01 16:18:25 +0300

    Useless comments have been deleted.

--- Alpha.ReturnStatus.hpp
+++ Alpha.ReturnStatus.hpp
@@ -4,9 +4,8 @@ enum ReturnStatus {
 
     EVERYTHING_IS_GOOD      =  0,
     UNRECOGNIZABLE_SYMBOL   = -1,
-    //NOT_A_SIMBOL            = -2,
-    NULLPOINTER_ARGUMENT    = -3,
-    EMPTY_INTERVAL          = -4
+    NULLPOINTER_ARGUMENT    = -2,
+    EMPTY_INTERVAL          = -3
 };
 
 const char* decodeReturnStatus(const ReturnStatus rs);
